16d0d571a4d43ff25fb56f6090025d6e0fba6cbe
Patrick <67674464+Squirrel10@users.noreply.github.com>
2021-03-21 18:00:31 -0700

    Add files via upload

---
+++ weeks/week10/includes/header.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>The Final Project!</title>
+<link href="css/styles.css" type="text/css" rel="stylesheet">
+</head>
+<header>
+    
+
+<div class="inner-header">
+    
+
+
+<ul>
+<?php
+echo makeLinks($nav);   
+?>	  
+</ul>	
+    
+
+    
+    
+</div>
+    
+    
+    
+    
+    
+    
+</header>
+<body>
+    
+    
+</body>
+    
+    
+<div id="wrapper">
+    
+    
+    
